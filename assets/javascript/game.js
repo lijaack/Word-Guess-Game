e1702300209278f132192c5cfe5361e3ec3c3819
Jack <lijaack@users.noreply.github.com>
2018-11-06 22:37:52 -0800

    added win/lose mechanics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,29 +5,43 @@ var randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.leng
 // check the randomWord length
 var positions = randomWord.length;
 //make new string with _
-var underscores =[]
+var underscores =[];
 for (i=0; positions>i; i++){
     underscores.push("_")
 }
+//wrong guess
+var wrongGuess =[];
 //grab html element
 var wordGuess = document.getElementById("word-guess");
+var wrongGuesses = document.getElementById("wrong-guesses");
 //push _ placements into html element
-    wordGuess.textContent = "Guess the Word: " + underscores;
+    wordGuess.textContent = underscores.join(" ");
 
 //user guesses a letter
 document.onkeyup = function(event) { 
-    var userGuess = event.key;
-//for this letter, compare it with the letters in the random word
-    if (randomWord.indexOf(userGuess)>-1) {
-//if the letter matches, push the letter into html position
-        underscores [randomWord.indexOf(userGuess)] = userGuess;
-        wordGuess.textContent = "Guess the Word: " + underscores;
+
+        var userGuess = event.key;
+
+    
+        //for this letter, compare it with the letters in the random word
+        if (randomWord.indexOf(userGuess)>-1) {
+
+            //if the letter matches, replace _ with letter and push to html
+            underscores [randomWord.indexOf(userGuess)] = userGuess;
+            wordGuess.textContent = underscores.join(" ");
+            }
+
+        //if the letter does not match, move letter to "wrong guesses" box.   
+        else if (wrongGuess.indexOf(userGuess)<0){
+        wrongGuess.push(userGuess);
+        wrongGuesses.textContent = wrongGuess.join(" ")
+        }
+        var wrongGuessCount = wrongGuess.join("");
+        var winWord = underscores.join("");
+        if (winWord===randomWord){
+            alert("YOU WIN!");
+        }
+        else if(wrongGuessCount.length===5){
+            alert("YOU LOSE!");
+        }
     }
-}
-
-
-// if the user's guess is a letter in the random word, replace the content in the "answer".
-// var index=randomWord.indexOf(userGuess);
-// if (index>-1) {
-//     answer[index]=userGuess;
-//

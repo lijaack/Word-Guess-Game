5e21e145f901f71534243033e4890242ef3ec21b
Jack <lijaack@users.noreply.github.com>
2018-11-07 22:24:43 -0800

    finished basic game mechanics
    need to add picture change after a win
    maybe add some sound effects as well

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,133 @@
-//list of possible words
-var randomWordArray =["aliens", "earth", "stars", "meteor", "venus", "jupiter", "planet", "galaxy", "universe"];
-//generate a random word from the array
-var randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
-// check the randomWord length
-var positions = randomWord.length;
-//make new string with _
-var underscores =[];
-for (i=0; positions>i; i++){
-    underscores.push("_")
-}
-//wrong guess array
-var wrongGuess =[];
-//grab html element
-var wordGuess = document.getElementById("word-guess");
-var wrongGuesses = document.getElementById("wrong-guesses");
-//push _ placements into html element
-    wordGuess.textContent = underscores.join(" ");
-
-// user guesses a letter
-document.onkeyup = function(event) { 
-
-    var userGuess = event.key;
+var wins = 0;
+var loses = 0;
 
 
-    //for this letter, compare it with the letters in the random word
-    if (randomWord.indexOf(userGuess)>-1) {
 
-        //if the letter matches, replace _ with letter and push to html
-        underscores [randomWord.indexOf(userGuess)] = userGuess;
+    //list of possible words
+    var randomWordArray =["abaaeebeaaabeeebbbb", "meteor", "universe"];
+    //generate a random word from the array
+    var randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
+    // check the randomWord length
+    var positions = randomWord.length;
+    //make new string with _
+    var underscores =[];
+    for (i=0; positions>i; i++){
+        underscores.push("_")
+    }
+    //wrong guess array
+    var wrongGuess =[];
+    //grab html element
+    var wordGuess = document.getElementById("word-guess");
+    var wrongGuesses = document.getElementById("wrong-guesses");
+    var wrongGuessesLeft = document.getElementById("wrong-guesses-left");
+    var winCount = document.getElementById("wincount");
+    var loseCount = document.getElementById("losecount");
+    //push _ placements into html element
         wordGuess.textContent = underscores.join(" ");
-        }
 
-    //if the letter does not match, move letter to "wrong guesses" box.   
-    else if (wrongGuess.indexOf(userGuess)<0){
-        wrongGuess.push(userGuess);
-        wrongGuesses.textContent = wrongGuess.join(" ")
+    // user guesses a letter
+
+    document.onkeyup = function(event) { 
+        var userGuess = event.key;
+
+        var indexes = [];
+
+
+        for(i = 0; i < randomWord.length; i++){
+            if (randomWord[i] === userGuess)
+                indexes.push(i);
+            }
+
+        for(a=0; a<=indexes.length; a++) {
+            if  (indexes[a]>-1) {
+                underscores[indexes[a]] = userGuess;
+                wordGuess.textContent = underscores.join(" ");
+            }
+
+            else if (wrongGuess.indexOf(userGuess)<0 && randomWord.indexOf(userGuess)<0 && userGuess.length===1){
+            wrongGuess.push(userGuess);
+            wrongGuesses.textContent = wrongGuess.join(" ")
+            wrongGuessesLeft.textContent = 5 - wrongGuess.length; 
+            }
         }
-       
-        // if you guess all the letters of the word you win!
+    // if you guess all the letters of the word you win!
         if (underscores.join("")===randomWord){
-            alert("YOU WIN!");
-        }
-        // 5 wrong guesses and you lose!
+                wins++;
+                winCount.textContent = wins;
+                reset()
+            }
+    // 5 wrong guesses and you lose!
         else if(wrongGuess.join("").length>=5){
-            alert("YOU LOSE!");
+                loses++;
+                loseCount.textContent = loses;
+                reset()
         }
     }
+
+
+function reset(){
+    //list of possible words
+    var randomWordArray =["abaaeebeaaabeeebbbb", "meteor", "universe"];
+    //generate a random word from the array
+    var randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
+    // check the randomWord length
+    var positions = randomWord.length;
+    //make new string with _
+    var underscores =[];
+    for (i=0; positions>i; i++){
+        underscores.push("_")
+    }
+    //wrong guess array
+    var wrongGuess =[];
+    //grab html element
+    var wordGuess = document.getElementById("word-guess");
+    var wrongGuesses = document.getElementById("wrong-guesses");
+    var wrongGuessesLeft = document.getElementById("wrong-guesses-left");
+    var winCount = document.getElementById("wincount");
+    var loseCount = document.getElementById("losecount");
+    wrongGuesses.textContent = "";
+    wrongGuessesLeft.textContent="5";
+
+    //push _ placements into html element
+        wordGuess.textContent = underscores.join(" ");
+
+    // user guesses a letter
+
+    document.onkeyup = function(event) { 
+        var userGuess = event.key;
+
+        var indexes = [];
+
+
+        for(i = 0; i < randomWord.length; i++){
+            if (randomWord[i] === userGuess)
+                indexes.push(i);
+            }
+
+        for(a=0; a<=indexes.length; a++) {
+            if  (indexes[a]>-1) {
+                underscores[indexes[a]] = userGuess;
+                wordGuess.textContent = underscores.join(" ");
+            }
+
+            else if (wrongGuess.indexOf(userGuess)<0 && randomWord.indexOf(userGuess)<0 && userGuess.length===1){
+            wrongGuess.push(userGuess);
+            wrongGuesses.textContent = wrongGuess.join(" ")
+            wrongGuessesLeft.textContent = 5 - wrongGuess.length; 
+            }
+        }
+    // if you guess all the letters of the word you win!
+        if (underscores.join("")===randomWord){
+                wins++;
+                winCount.textContent = wins;
+                reset()
+            }
+    // 5 wrong guesses and you lose!
+        else if(wrongGuess.join("").length>=5){
+                loses++;
+                loseCount.textContent = loses;
+                reset()
+        }
+    }
+
+}

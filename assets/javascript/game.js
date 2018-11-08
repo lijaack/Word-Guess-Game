ddee841fe02c15ea958739e766fab0d882d0f8d2
Jack <lijaack@users.noreply.github.com>
2018-11-07 17:00:01 -0800

    Update assets/javascript/game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,29 +20,29 @@ var wrongGuesses = document.getElementById("wrong-guesses");
 // user guesses a letter
 document.onkeyup = function(event) { 
 
-        var userGuess = event.key;
+    var userGuess = event.key;
 
-    
-        //for this letter, compare it with the letters in the random word
-        if (randomWord.indexOf(userGuess)>-1) {
 
-            //if the letter matches, replace _ with letter and push to html
-            underscores [randomWord.indexOf(userGuess)] = userGuess;
-            wordGuess.textContent = underscores.join(" ");
-            }
+    //for this letter, compare it with the letters in the random word
+    if (randomWord.indexOf(userGuess)>-1) {
 
-        //if the letter does not match, move letter to "wrong guesses" box.   
-        else if (wrongGuess.indexOf(userGuess)<0){
+        //if the letter matches, replace _ with letter and push to html
+        underscores [randomWord.indexOf(userGuess)] = userGuess;
+        wordGuess.textContent = underscores.join(" ");
+        }
+
+    //if the letter does not match, move letter to "wrong guesses" box.   
+    else if (wrongGuess.indexOf(userGuess)<0){
         wrongGuess.push(userGuess);
         wrongGuesses.textContent = wrongGuess.join(" ")
         }
-       
-        // if you guess all the letters of the word you win!
-        if (underscores.join("")===randomWord){
-            alert("YOU WIN!");
-        }
-        // 5 wrong guesses and you lose!
-        else if(wrongGuess.join("").length===5){
-            alert("YOU LOSE!");
-        }
+    
+    // if you guess all the letters of the word you win!
+    if (underscores.join("")===randomWord){
+        alert("YOU WIN!");
     }
+    // 5 wrong guesses and you lose!
+    else if(wrongGuess.join("").length===5){
+        alert("YOU LOSE!");
+    }
+} 

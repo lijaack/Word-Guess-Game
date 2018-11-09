b20dd80d3294246c81d2b6f562d025f3ee137409
Jack <lijaack@users.noreply.github.com>
2018-11-08 16:45:16 -0800

    added "alphabets only" mechanics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var wins = 0;
 var loses = 0;
-
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
 
 
     //list of possible words
@@ -22,49 +22,51 @@ var loses = 0;
     var wrongGuessesLeft = document.getElementById("wrong-guesses-left");
     var winCount = document.getElementById("wincount");
     var loseCount = document.getElementById("losecount");
-    //push _ placements into html element
+    //content shown when game starts
         wordGuess.textContent = underscores.join(" ");
-
+        wrongGuesses.textContent = "";
+        wrongGuessesLeft.textContent="5";
     // user guesses a letter
    
         document.onkeyup = function(event) { 
             if (underscores.join("")!==randomWord && wrongGuess.length<5){
-            var userGuess = event.key;
+                var userGuessN = event.key;
+                var userGuess = userGuessN.toLowerCase();
+                var indexes = [];
 
-            var indexes = [];
+                if (alphabet.indexOf(userGuess)>-1){
+                    for(i = 0; i < randomWord.length; i++){
+                        if (randomWord[i] === userGuess)
+                            indexes.push(i);
+                        }
 
+                    for(a=0; a<=indexes.length; a++) {
+                        if  (indexes[a]>-1) {
+                            underscores[indexes[a]] = userGuess;
+                            wordGuess.textContent = underscores.join(" ");
+                        }
 
-            for(i = 0; i < randomWord.length; i++){
-                if (randomWord[i] === userGuess)
-                    indexes.push(i);
+                        else if (wrongGuess.indexOf(userGuess)<0 && randomWord.indexOf(userGuess)<0 && userGuess.length===1){
+                        wrongGuess.push(userGuess);
+                        wrongGuesses.textContent = wrongGuess.join(" ");
+                        wrongGuessesLeft.textContent = 5 - wrongGuess.length; 
+                        }
+                    }
+                // if you guess all the letters of the word you win!
+                    if (underscores.join("")===randomWord){
+                            wins++;
+                            winCount.textContent = wins;
+                            setTimeout(reset, 1000);
+                        }
+                // 5 wrong guesses and you lose!
+                    else if(wrongGuess.join("").length>=5){
+                            loses++;
+                            loseCount.textContent = loses;
+                            setTimeout(reset, 1000);
+                    }
                 }
-
-            for(a=0; a<=indexes.length; a++) {
-                if  (indexes[a]>-1) {
-                    underscores[indexes[a]] = userGuess;
-                    wordGuess.textContent = underscores.join(" ");
-                }
-
-                else if (wrongGuess.indexOf(userGuess)<0 && randomWord.indexOf(userGuess)<0 && userGuess.length===1){
-                wrongGuess.push(userGuess);
-                wrongGuesses.textContent = wrongGuess.join(" ")
-                wrongGuessesLeft.textContent = 5 - wrongGuess.length; 
-                }
-            }
-        // if you guess all the letters of the word you win!
-            if (underscores.join("")===randomWord){
-                    wins++;
-                    winCount.textContent = wins;
-                    setTimeout(reset, 1000);
-                }
-        // 5 wrong guesses and you lose!
-            else if(wrongGuess.join("").length>=5){
-                    loses++;
-                    loseCount.textContent = loses;
-                    setTimeout(reset, 1000);
-            }
+            }   
         }
-    }
 // =============================================================== reset game ===============================================================
 
 function reset(){
@@ -87,51 +89,54 @@ function reset(){
     var wrongGuessesLeft = document.getElementById("wrong-guesses-left");
     var winCount = document.getElementById("wincount");
     var loseCount = document.getElementById("losecount");
-    wrongGuesses.textContent = "";
-    wrongGuessesLeft.textContent="5";
 
-    //push _ placements into html element
+
+    //content shown when game starts
         wordGuess.textContent = underscores.join(" ");
+        wrongGuesses.textContent = "";
+        wrongGuessesLeft.textContent="5";
 
     // user guesses a letter
 
     document.onkeyup = function(event) { 
         if (underscores.join("")!==randomWord && wrongGuess.length<5){
-            var userGuess = event.key;
 
+            var userGuessN = event.key;
+            var userGuess = userGuessN.toLowerCase();
             var indexes = [];
 
+            if (alphabet.indexOf(userGuess)>-1){
+                for(i = 0; i < randomWord.length; i++){
+                    if (randomWord[i] === userGuess)
+                        indexes.push(i);
+                    }
 
-            for(i = 0; i < randomWord.length; i++){
-                if (randomWord[i] === userGuess)
-                    indexes.push(i);
+                for(a=0; a<=indexes.length; a++) {
+                    if  (indexes[a]>-1) {
+                        underscores[indexes[a]] = userGuess;
+                        wordGuess.textContent = underscores.join(" ");
+                    }
+
+                    else if (wrongGuess.length<6 &&wrongGuess.indexOf(userGuess)<0 && randomWord.indexOf(userGuess)<0 && userGuess.length===1){
+                    wrongGuess.push(userGuess);
+                    wrongGuesses.textContent = wrongGuess.join(" ")
+                    wrongGuessesLeft.textContent = 5 - wrongGuess.length; 
+                    }
                 }
-
-            for(a=0; a<=indexes.length; a++) {
-                if  (indexes[a]>-1) {
-                    underscores[indexes[a]] = userGuess;
-                    wordGuess.textContent = underscores.join(" ");
+                // if you guess all the letters of the word you win!
+                if (underscores.join("")===randomWord){
+                        wins++;
+                        winCount.textContent = wins;
+                        setTimeout(reset, 2000);
                 }
-
-                else if (wrongGuess.length<6 &&wrongGuess.indexOf(userGuess)<0 && randomWord.indexOf(userGuess)<0 && userGuess.length===1){
-                wrongGuess.push(userGuess);
-                wrongGuesses.textContent = wrongGuess.join(" ")
-                wrongGuessesLeft.textContent = 5 - wrongGuess.length; 
+                // 5 wrong guesses and you lose!
+                else if(wrongGuess.join("").length>=5){
+                        loses++;
+                        loseCount.textContent = loses;
+                        setTimeout(reset, 2000);
                 }
             }
-            // if you guess all the letters of the word you win!
-            if (underscores.join("")===randomWord){
-                    wins++;
-                    winCount.textContent = wins;
-                    setTimeout(reset, 2000);
-            }
-            // 5 wrong guesses and you lose!
-            else if(wrongGuess.join("").length>=5){
-                    loses++;
-                    loseCount.textContent = loses;
-                    setTimeout(reset, 2000);
-            }
+
         }
-
-    }
+    }   
 }
